377e52ad3e2caecb0e831b4c4ea044250dd86803
Pedro Reis <59806642+Paywolf@users.noreply.github.com>
2020-04-21 20:37:53 -0400

    1

--- src/main/java/bsu/comp152/DataHandler.java
+++ src/main/java/bsu/comp152/DataHandler.java
@@ -1,4 +1,24 @@
 package bsu.comp152;
 
+import java.util.ArrayList;
+
 public class DataHandler {
+
+    class responseDataType {
+        String title;
+        float version;
+        String href;
+        ArrayList<recipeDataType> results;
+
+    }
+    class recipeDataType {
+        String title;
+        String href;
+        String ingredients;
+        String thumbnail;
+        @Override
+        public String toString() {
+            return "Recipe Title:" + title ; // only title for display later
+        }
+    }
 }
